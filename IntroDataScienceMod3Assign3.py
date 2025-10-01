ebe64f8d5815f57e97a14cc2e246556124bd7e56
MantakSingh <44886815+MantakSingh@users.noreply.github.com>
2025-10-01 16:37:16 -0700

    Update IntroDataScienceMod3Assign3.py

--- IntroDataScienceMod3Assign3.py
+++ IntroDataScienceMod3Assign3.py
@@ -92,3 +92,56 @@ def answer_one():
     ]
        
     return df
+       
+def answer_two():
+       
+    ##################################################################
+                        ## Energy Dataframe
+    ##################################################################
+    
+    Energy = pd.read_excel('assets/Energy Indicators.xls',
+                           na_values=["..."],header = None,
+                           skiprows=18,
+                           skipfooter= 38,
+                           usecols=[2,3,4,5],
+                           names=['Country', 'Energy Supply', 'Energy Supply per Capita', '% Renewable'])
+    Energy['Energy Supply'] = Energy['Energy Supply'].apply(lambda x: x*1000000)
+
+    Energy['Country'] = Energy['Country'].str.replace(r" \(.*\)","")
+    Energy['Country'] = Energy['Country'].str.replace(r"\d*","")
+    Energy['Country'] = Energy['Country'].replace(
+        {'Republic of Korea' : 'South Korea',
+         'United States of America' : 'United States',
+         'United Kingdom of Great Britain and Northern Ireland':'United Kingdom',
+         'China, Hong Kong Special Administrative Region':'Hong Kong'
+        })
+    
+    ##################################################################
+                        ## GDP Dataframe
+    ##################################################################
+    
+    GDP = pd.read_csv('assets/world_bank.csv', skiprows = 4)
+    GDP['Country Name'] = GDP['Country Name'].replace(
+        {'Korea, Rep.': 'South Korea', 
+         'Iran, Islamic Rep.': 'Iran', 
+         'Hong Kong SAR, China' : 'Hong Kong'})
+    GDP.rename(columns = {"Country Name":"Country"},inplace=True)
+    GDP = GDP.loc[:,['2006', '2007', '2008', '2009', '2010', '2011', '2012', '2013', '2014', '2015',"Country"]]
+       
+    ##################################################################
+                        ## ScimEn Dataframe
+    ##################################################################
+    
+    ScimEn = pd.read_excel('assets/scimagojr-3.xlsx')
+    
+    ##################################################################
+                        ## Combined Dataframe
+    ##################################################################
+    
+    inner1 = pd.merge(ScimEn,Energy,how="inner",left_on="Country",right_on="Country")
+    inner2 = pd.merge(inner1,GDP,how="inner",left_on="Country",right_on="Country").set_index("Country")
+
+    outer1 = pd.merge(ScimEn,Energy,how="outer",left_on="Country",right_on="Country")
+    outer2 = pd.merge(outer1,GDP,how="outer",left_on="Country",right_on="Country").set_index("Country")
+
+    return len(outer2)-len(inner2)
