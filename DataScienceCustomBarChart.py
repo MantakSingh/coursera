239db80c65c6dbf5bd3f80b2d4c5b33d00a8ce31
MantakSingh <44886815+MantakSingh@users.noreply.github.com>
2025-10-06 17:29:56 -0700

    Update DataScienceCustomBarChart.py

--- DataScienceCustomBarChart.py
+++ DataScienceCustomBarChart.py
@@ -14,21 +14,24 @@ df = pd.DataFrame([np.random.normal(32000,200000,3650),
                   index=[1992,1993,1994,1995])
 df_index = df.index
 row_averages = df.mean(axis=1)
+std_dev_df = df.sem(axis=1)  # Standard Error of Mean(Standard Deviation is huge)
 
 ##############
  # User Input
 ##############
 
-user_y_input = input("What y-value do you want?")
-user_y_input = float(user_y_input)
-plt.axhline(y = user_y_input, color='b', linestyle='--')
-
+try:
+    user_y_input = float(input("Enter a y-value: "))
+except ValueError:
+    print("Invalid input. Defaulting to 0.")
+    user_y_input = 0.0
+    
 ##############
  # Bar Color
 ##############
 
 def color_for_value(value, threshold):
-    """Return bar color based on comparison to threshold."""
+    "Return bar color based on comparison to threshold."
     if value > threshold:
         return 'red'
     elif value < threshold:
@@ -44,6 +47,7 @@ bar_colors = [color_for_value(v, user_y_input) for v in row_averages]
 
 plt.bar(row_averages.index, row_averages, color=bar_colors)
 plt.axhline(y=user_y_input, color='black', linestyle='--', label=f'y = {user_y_input}')
+plt.errorbar(row_averages.index, row_averages, yerr=std_dev_df, fmt="", color="r", capthick = 1)
 
 plt.title("Bar Chart")
 plt.xlabel('Years')
