2b6415e40fd91c9235d9603173c8bbafb6de9289
MantakSingh <44886815+MantakSingh@users.noreply.github.com>
2025-10-06 18:00:46 -0700

    Update DataScienceCustomBarChart.py

--- DataScienceCustomBarChart.py
+++ DataScienceCustomBarChart.py
@@ -1,6 +1,8 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
+from matplotlib.cm import ScalarMappable
+from matplotlib.colors import Normalize
 
 ##############
  # Load Data
@@ -13,8 +15,11 @@ df = pd.DataFrame([np.random.normal(32000,200000,3650),
                    np.random.normal(48000,70000,3650)], 
                   index=[1992,1993,1994,1995])
 df_index = df.index
-row_averages = df.mean(axis=1)
-sem_df = df.sem(axis=1)  # Standard Error of Mean(Standard Deviation is huge)
+
+# Information about the dataframe
+row_averages = df.mean(axis=1) # Averages
+sem_df = df.sem(axis=1)  # Standard Error of Mean
+z = (row_averages - user_y_input) / sem_df  # how many SEMs away
 
 ##############
  # User Input
@@ -30,16 +35,13 @@ except ValueError:
  # Bar Color
 ##############
 
-def color_for_value(value, threshold):
-    "Return bar color based on comparison to threshold."
-    if value > threshold:
-        return 'red'
-    elif value < threshold:
-        return 'blue'
-    return 'yellow'
-    
-# Setting Colors
-bar_colors = [color_for_value(v, user_y_input) for v in row_averages]
+max_z = 3
+cmap = plt.cm.seismic  # Blue/White/Red
+norm = Normalize(vmin=-max_z, vmax=max_z)
+bar_colors = cmap(norm(z))
+
+sm = ScalarMappable(cmap=cmap, norm=norm)
+sm.set_array([])  # required for colorbar
 
 ##############
     # Plot
@@ -48,7 +50,7 @@ bar_colors = [color_for_value(v, user_y_input) for v in row_averages]
 plt.bar(
     row_averages.index,
     row_averages,
-    color=bar_colors,
+    color = bar_colors,
     yerr=sem_df,
     capsize=5,
     ecolor='black'
@@ -58,10 +60,12 @@ plt.axhline(
     y=user_y_input,
     color='black',
     linestyle='--',
-    label=f'y = {user_y_input}'
+    label=f'Desired y-value = {user_y_input}'
 )
 
+
 plt.title("Bar Chart")
+plt.colorbar(sm, label='Distance from user line (in SEMs)', orientation='horizontal')
 plt.xlabel('Years')
 plt.xticks([1992,1993,1994,1995])
 plt.ylabel('Values')
