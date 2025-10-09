2f9e50ae46fbc10ba614871e44421f188bba85cd
MantakSingh <44886815+MantakSingh@users.noreply.github.com>
2025-10-08 19:09:15 -0700

    Create AppliedPlottingFinal.py

---
+++ AppliedPlottingFinal.py
@@ -0,0 +1,5 @@
+import pandas as pd
+import numpy as np
+import matplotlib as plt
+
+femaledeath_df = pd.read_excel(r"C:\Users\kensi\Downloads\Intentional homicide victims by sex, counts and ra.xls")
